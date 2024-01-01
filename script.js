9ca0a747f061ff27badfd1ce842f9e94c0bfbfd3
roysonmenezes <royson32004@gmail.com>
2024-01-01 22:42:28 +0530

    edited css and js file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let userInput=document.getElementById("date");
 userInput.max=new Date().toISOString().split("T")[0];
 
+let result=document.getElementById("result");
+
 function calculateAge(){
     let birthDate=new Date(userInput.value);
 
@@ -14,10 +16,10 @@ function calculateAge(){
     let m2=today.getMonth()+1;
     let y2=today.getFullYear();
 
-    let d3,m3,y3;
+    let d3, m3, y3;
     y3=y2-y1;
 
-    if(m2>m1){
+    if(m2>=m1){
         m3=m2-m1;
     }else{
         y3--;
@@ -35,7 +37,10 @@ function calculateAge(){
         m3=11;
         y3--;
     }
-    console.log(y3,m3,d3)
+
+    result.innerHTML=`You are <span>${y3}</span> years,<span>${m3}</span> months and,  <span>${d3}</span> days old`;
+
+    
 
 }
 function getDaysInMonth(year,month){
